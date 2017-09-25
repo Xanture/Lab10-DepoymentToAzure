3a85b491cbc0b5497221bbbe67f19db6b053f4bf
Brian McFarlane <bmcfarlane82@gmail.com>
2017-09-25 11:40:19 -0700

    Added readme

---
+++ lab10_brian/lab10_brian/Pages/Index.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace lab10_brian.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
